7121eaeb87bd4f4388294d86f14e4e8ad73cd813
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2023-11-04 20:35:03 +1000

    nn

--- main_component.js
+++ main_component.js
@@ -12,27 +12,27 @@ export default {
   template: `
     <ul class="nav nav-tabs" id="controlTabs" role="tablist">
   <li class="nav-item" role="presentation">
-    <button class="nav-link active" id="remote-control-tab" data-bs-toggle="tab" data-bs-target="#remote-control" type="button" role="tab" aria-controls="remote-control" aria-selected="true">Key Control</button>
+    <button class="nav-link active" id="remote-control-tabs" data-bs-toggle="tab" data-bs-target="#remote-controls" type="button" role="tab" aria-controls="remote-controls" aria-selected="true">Key Control</button>
   </li>
   <li class="nav-item" role="presentation">
-    <button class="nav-link" id="button-control-tab" data-bs-toggle="tab" data-bs-target="#button-control" type="button" role="tab" aria-controls="button-control" aria-selected="false">Button Control</button>
+    <button class="nav-link" id="button-control-tabs" data-bs-toggle="tab" data-bs-target="#button-controls" type="button" role="tab" aria-controls="button-controls" aria-selected="false">Button Control</button>
   </li>
   <li class="nav-item" role="presentation">
-    <button class="nav-link" id="code-control-tab" data-bs-toggle="tab" data-bs-target="#code-control" type="button" role="tab" aria-controls="code-control" aria-selected="false">Code Control</button>
+    <button class="nav-link" id="code-control-tasb" data-bs-toggle="tab" data-bs-target="#code-controls" type="button" role="tab" aria-controls="code-controls" aria-selected="false">Code Control</button>
   </li>
 </ul>
 
 
 <div class="tab-content" id="controlTabsContent">
 
-  <div id="remote-control" class="tab-pane fade show active" role="tabpanel" aria-labelledby="remote-control-tab">
+  <div id="remote-controls" class="tab-pane fade show active" role="tabpanel" aria-labelledby="remote-control-tabs">
   </div>
 
-  <div id="button-control" class="tab-pane fade" role="tabpanel" aria-labelledby="button-control-tab">
+  <div id="button-controls" class="tab-pane fade" role="tabpanel" aria-labelledby="button-control-tabs">
     <ButtonControlComponent/>
   </div>
 
-  <div class="tab-pane fade" id="code-control" role="tabpanel" aria-labelledby="code-control-tab">
+  <div class="tab-pane fade" id="code-controls" role="tabpanel" aria-labelledby="code-control-tabs">
   </div>
 
 </div>

c1c8a83d7ce073c23d3b326bdacd860bac6f9e97
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2023-11-05 10:20:44 +1000

    nn

--- key_control_component.js
+++ key_control_component.js
@@ -3,17 +3,18 @@ import keyConfigs from './configs.js'
 
 export default {
   setup() {
-    let selectedConfig = ref({name: "none", instructions: "#", config: [{key:38,action:'motorbc',direction:'forward',speed:100}]})
+    const valueKeyMap = {
+        38:'↑',40:'↓',37:'←',39:'→',32:'spacebar',65:'a',66:'b',67:'c',68:'d',69:'e',70:'f',71:'g',72:'h',73:'i',74:'j',75:'k',76:'l',77:'m',78:'n',79:'o',80:'p',81:'q',82:'r',83:'s',84:'t',85:'u',86:'v',87:'w',88:'x',89:'y',90:'z',49:'1',50:'2',51:'3',52:'4',53:'5',54:'6',55:'7',56:'8',57:'9',48:'0'
+      };
+    let selectedConfig = ref({name: "", instructions: "#", config: []})
     let selectedConfigName = ref("none")
-    return { NXTConstants, keyConfigs, selectedConfig, selectedConfigName }
+    return { NXTConstants, keyConfigs, selectedConfig, selectedConfigName, valueKeyMap }
   },
   methods: {
     onConfigChange(event) {
-        console.log(this.selectedConfigName)
         for (const config of keyConfigs.keyConfigs) {
             if (this.selectedConfigName === config.name) {
                 this.selectedConfig = config
-                console.log(config)
             }
         }
     }
@@ -38,7 +39,7 @@ export default {
       <p>Config</p>
       <div>
         <p v-for="(item in selectedConfig.config">
-          {{ item.key }} {{ item.action }} {{ item.direction }} {{ item.speed }}
+          {{ valueKeyMap[item.key] }} {{ item.action }} {{ item.direction }} {{ item.speed }}
         </p>
       </div>
     </div>

f4cdd5f456897a32a64a64fd7db016289ef55754
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2023-11-06 11:46:56 +1000

    nn

--- main_component.js
+++ main_component.js
@@ -70,32 +70,35 @@ export default {
     <a href="https://github.com/lance-gilmore/lance-gilmore.github.io">github repo</a>
   </div>
 
-<NxtInfoComponent :deviceName="deviceName" :bluetoothAddress="bluetoothAddress" :firmwareVersion="firmwareVersion" :protocolVersion="protocolVersion" :batteryLevelMillivolts="batteryLevelMillivolts" :batteryPercent="batteryPercent" />
+<div class="view2 mt-4 d-none">
+  <NxtInfoComponent :deviceName="deviceName" :bluetoothAddress="bluetoothAddress" :firmwareVersion="firmwareVersion" :protocolVersion="protocolVersion" :batteryLevelMillivolts="batteryLevelMillivolts" :batteryPercent="batteryPercent" />
 
-<ul class="nav nav-tabs" id="controlTabs" role="tablist">
-  <li class="nav-item" role="presentation">
-    <button class="nav-link active" id="remote-control-tabs" data-bs-toggle="tab" data-bs-target="#remote-controls" type="button" role="tab" aria-controls="remote-controls" aria-selected="true">Key Control</button>
-  </li>
-  <li class="nav-item" role="presentation">
-    <button class="nav-link" id="button-control-tabs" data-bs-toggle="tab" data-bs-target="#button-controls" type="button" role="tab" aria-controls="button-controls" aria-selected="false">Button Control</button>
-  </li>
-  <li class="nav-item" role="presentation">
-    <button class="nav-link" id="code-control-tasb" data-bs-toggle="tab" data-bs-target="#code-controls" type="button" role="tab" aria-controls="code-controls" aria-selected="false">Code Control</button>
-  </li>
-</ul>
+  <ul class="nav nav-tabs" id="controlTabs" role="tablist">
+    <li class="nav-item" role="presentation">
+      <button class="nav-link active" id="remote-control-tabs" data-bs-toggle="tab" data-bs-target="#remote-controls" type="button" role="tab" aria-controls="remote-controls" aria-selected="true">Key Control</button>
+    </li>
+    <li class="nav-item" role="presentation">
+      <button class="nav-link" id="button-control-tabs" data-bs-toggle="tab" data-bs-target="#button-controls" type="button" role="tab" aria-controls="button-controls" aria-selected="false">Button Control</button>
+    </li>
+    <li class="nav-item" role="presentation">
+      <button class="nav-link" id="code-control-tasb" data-bs-toggle="tab" data-bs-target="#code-controls" type="button" role="tab" aria-controls="code-controls" aria-selected="false">Code Control</button>
+    </li>
+  </ul>
 
-<div class="tab-content" id="controlTabsContent">
+  <div class="tab-content" id="controlTabsContent">
 
-  <div id="remote-controls" class="tab-pane fade show active" role="tabpanel" aria-labelledby="remote-control-tabs">
-  <KeyControlComponent/>
-  </div>
+    <div id="remote-controls" class="tab-pane fade show active" role="tabpanel" aria-labelledby="remote-control-tabs">
+    <KeyControlComponent/>
+    </div>
 
-  <div id="button-controls" class="tab-pane fade" role="tabpanel" aria-labelledby="button-control-tabs">
-    <ButtonControlComponent/>
-  </div>
+    <div id="button-controls" class="tab-pane fade" role="tabpanel" aria-labelledby="button-control-tabs">
+      <ButtonControlComponent/>
+    </div>
 
-  <div class="tab-pane fade" id="code-controls" role="tabpanel" aria-labelledby="code-control-tabs">
-  <CodeControlComponent/>
+    <div class="tab-pane fade" id="code-controls" role="tabpanel" aria-labelledby="code-control-tabs">
+    <CodeControlComponent/>
+    </div>
+
   </div>
 
 </div>

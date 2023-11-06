0680363fc6ec07e14a536ac4dd3056a11d7acefb
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2023-11-06 11:43:04 +1000

    nn

--- main_component.js
+++ main_component.js
@@ -12,11 +12,64 @@ export default {
         NxtInfoComponent
       },
   setup() {
+    let deviceName = ref('')
+    let bluetoothAddress = ref('')
+    let firmwareVersion = ref('')
+    let protocolVersion = ref('')
+    let batteryLevelMillivolts = ref('')
+    let batteryPercent = ref('')
+
     return { NXTConstants }
   },
-  props: ['deviceName', 'bluetoothAddress', 'firmwareVersion', 'protocolVersion', 'batteryLevelMillivolts', 'batteryPercent'],
+  methods: {
+    async connectNxt() {
+      await connectDeviceSerial();
+      // TOOD: the stuff below on actual connect eventrunCom
+      $('.view1').addClass('d-none');
+      $('.view2').removeClass('d-none');
+      
+      runCommandQueue();
+      this.getAllInfo();
+    },
+    async getAllInfo() {
+      addCommandToQueue(function() {
+        getInfo(async function(res) {
+          this.deviceName = res.deviceName
+          this.bluetoothAddress = res.bluetoothAddress
+        });
+      });
+      addCommandToQueue(function() {
+        getVersion(async function(res) {
+          this.firmwareVersion = res.firmwareVersion
+          this.protocolVersion = res.protocolVersion
+        });
+      });
+      addCommandToQueue(function() {
+        getBatteryLevel(async function(res) {
+          this.batteryLevelMillivolts = res.batteryLevelMillivolts
+          this.batteryPercent = res.batteryPercent
+          initSensors();
+        });
+      });
+    }
+  },
 
   template: `
+  <div class="view1">
+    <button class="btn btn-primary" type="button" id="connectDeviceBtn" @click="connectNxt">connect</button>
+    <p>Ubuntu serial connect:
+    <ul>
+    <li>in terminal:</li>
+    <li>sudo adduser lance dialout (then restart machine)</li>
+    <li>rfcomm bind /dev/rfcomm0 00:16:53:0D:F6:08 1</li>
+    <li>test/complete connection with:</li>
+    <li>sudo l2ping 00:16:53:0D:F6:08</li>
+    <li>should be able to connect from chrome now (may need to change access on rfcomm so you dont have to run command as sudo)</li>
+    </ul>
+    </p>
+    <a href="https://github.com/lance-gilmore/lance-gilmore.github.io">github repo</a>
+  </div>
+
 <NxtInfoComponent :deviceName="deviceName" :bluetoothAddress="bluetoothAddress" :firmwareVersion="firmwareVersion" :protocolVersion="protocolVersion" :batteryLevelMillivolts="batteryLevelMillivolts" :batteryPercent="batteryPercent" />
 
 <ul class="nav nav-tabs" id="controlTabs" role="tablist">

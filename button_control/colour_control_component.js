e2846358783c854c5d917b4e214ad5cde86b83ec
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2023-11-08 10:57:54 +1000

    nn

--- button_control/colour_control_component.js
+++ button_control/colour_control_component.js
@@ -13,8 +13,8 @@ export default {
   },
   template: `
 <div class="my-4">
-    <h3>Light</h3>
-    <button :class="\`btn btn-${selectedColour} mx-1\`" type="button" @click="changeLight(NXTConstants.sensorTypes.COLOR_RED)">red</button>
+    <h3>Light</h3>{{ selectedColour }}
+    <button  type="button" @click="changeLight(NXTConstants.sensorTypes.COLOR_RED)">red</button>
     <button class="btn btn-secondary mx-1" type="button" @click="changeLight(NXTConstants.sensorTypes.COLOR_GREEN)">green</button>
     <button class="btn btn-secondary mx-1" type="button" @click="changeLight(NXTConstants.sensorTypes.COLOR_BLUE)">blue</button>
     <button class="btn btn-secondary mx-1" type="button" @click="changeLight(NXTConstants.sensorTypes.COLOR_FULL)">all</button>

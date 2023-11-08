dd81c16cd6671e33594d706693c51e347e79a4d5
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2023-11-08 10:36:41 +1000

    nn

--- button_control/colour_control_component.js
+++ button_control/colour_control_component.js
@@ -2,17 +2,19 @@ import { ref } from 'vue'
 
 export default {
   setup() {
-    return { NXTConstants }
+    let selectedColour = ref(NXTConstants.sensorTypes.COLOR_NONE)
+    return { NXTConstants, selectedColour }
   },
   methods: {
     async changeLight(colour) {
         await setInputModeColour(colour,colourPort);
+        selectedColour = colour
     }
   },
   template: `
 <div class="my-4">
     <h3>Light</h3>
-    <button class="btn btn-secondary mx-1" type="button" @click="changeLight(NXTConstants.sensorTypes.COLOR_RED)">red</button>
+    <button :class="\`btn btn-${ selectedColour === NXTConstants.sensorTypes.COLOR_RED ? 'danger' : 'secondary' } mx-1\`" type="button" @click="changeLight(NXTConstants.sensorTypes.COLOR_RED)">red</button>
     <button class="btn btn-secondary mx-1" type="button" @click="changeLight(NXTConstants.sensorTypes.COLOR_GREEN)">green</button>
     <button class="btn btn-secondary mx-1" type="button" @click="changeLight(NXTConstants.sensorTypes.COLOR_BLUE)">blue</button>
     <button class="btn btn-secondary mx-1" type="button" @click="changeLight(NXTConstants.sensorTypes.COLOR_FULL)">all</button>

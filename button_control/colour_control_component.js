2c15dd78f02200dcc61715c8bc52f1500416c5cd
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2023-11-08 10:45:48 +1000

    nn

--- button_control/colour_control_component.js
+++ button_control/colour_control_component.js
@@ -2,7 +2,7 @@ import { ref } from 'vue'
 
 export default {
   setup() {
-    let selectedColour = ref(NXTConstants.sensorTypes.COLOR_NONE)
+    const selectedColour = ref(NXTConstants.sensorTypes.COLOR_NONE)
     return { NXTConstants, selectedColour }
   },
   methods: {
@@ -14,7 +14,7 @@ export default {
   template: `
 <div class="my-4">
     <h3>Light</h3>
-    <button :class="\`btn btn-${ selectedColour === NXTConstants.sensorTypes.COLOR_RED ? 'danger' : 'secondary' } mx-1\`" type="button" @click="changeLight(NXTConstants.sensorTypes.COLOR_RED)">red</button>
+    <button :class="\`btn btn-${selectedColour === NXTConstants.sensorTypes.COLOR_RED ? 'danger' : 'secondary' } mx-1\`" type="button" @click="changeLight(NXTConstants.sensorTypes.COLOR_RED)">red</button>
     <button class="btn btn-secondary mx-1" type="button" @click="changeLight(NXTConstants.sensorTypes.COLOR_GREEN)">green</button>
     <button class="btn btn-secondary mx-1" type="button" @click="changeLight(NXTConstants.sensorTypes.COLOR_BLUE)">blue</button>
     <button class="btn btn-secondary mx-1" type="button" @click="changeLight(NXTConstants.sensorTypes.COLOR_FULL)">all</button>

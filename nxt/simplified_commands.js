8c3b52db0e7fc229d2243a526462a80ffbdeed2f
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2023-11-09 14:33:44 +1000

    nn

--- nxt/simplified_commands.js
+++ nxt/simplified_commands.js
@@ -1,15 +1,15 @@
-import NXT from './nxt.js'
+import NXTCommands from './commands.js'
 import NXTConstants from './config.js'
 
 export default {  
   async simpleBeep() {
-    await NXT.beep(523,500,false);
+    await NXTCommands.beep(523,500,false);
   },
 
   async stopMotor(motorPort) {
     const motorPowerPercent = 0; // 0 - 100 forwards 255 - 156 backwards (156 = 100% backwards)
     const regulatedTurnRatio = 0; // 0 - 100 forwards 255 - 156 backwards (156 = 100% backwards)
-    await NXT.runMotorCommand(motorPort,motorPowerPercent,
+    await NXTCommands.runMotorCommand(motorPort,motorPowerPercent,
       NXTConstants.motorModes.motorOnBreakRegulated,NXTConstants.motorRegulation.none,
       regulatedTurnRatio,NXTConstants.motorRunState.running,0);
   },
@@ -17,7 +17,7 @@ export default {
   async forwardMotor(motorPort) {
     const motorPowerPercent = 100; // 0 - 100 forwards 255 - 156 backwards (156 = 100% backwards)
     const regulatedTurnRatio = 0; // 0 - 100 forwards 255 - 156 backwards (156 = 100% backwards)
-    await NXT.runMotorCommand(motorPort,motorPowerPercent,
+    await NXTCommands.runMotorCommand(motorPort,motorPowerPercent,
       NXTConstants.motorModes.motorOnBreakRegulated,NXTConstants.motorRegulation.none,
       regulatedTurnRatio,NXTConstants.motorRunState.running,0);
   },
@@ -25,19 +25,19 @@ export default {
   async backwardMotor(motorPort) {
     const motorPowerPercent = 156; // 0 - 100 forwards 255 - 156 backwards (156 = 100% backwards)
     const regulatedTurnRatio = 0; // 0 - 100 forwards 255 - 156 backwards (156 = 100% backwards)
-    await NXT.runMotorCommand(motorPort,motorPowerPercent,
+    await NXTCommands.runMotorCommand(motorPort,motorPowerPercent,
       NXTConstants.motorModes.motorOnBreakRegulated,NXTConstants.motorRegulation.none,
       regulatedTurnRatio,NXTConstants.motorRunState.running,0);
   },
   
   async motorAOneRevolution() {
-    await NXT.turnMotorDegrees(NXTConstants.motors.PORT_A,360,true);
+    await NXTCommands.turnMotorDegrees(NXTConstants.motors.PORT_A,360,true);
   },
   
   async turnMotorSpeed(motorPort,speed,forward) {
     const motorPowerPercent = (forward) ? speed : 256 - speed; // 0 - 100 forwards 255 - 156 backwards (156 = 100% backwards)
     const regulatedTurnRatio = 0; // 0 - 100 forwards 255 - 156 backwards (156 = 100% backwards)
-    await NXT.runMotorCommand(motorPort,motorPowerPercent,
+    await NXTCommands.runMotorCommand(motorPort,motorPowerPercent,
       NXTConstants.motorModes.motorOnBreakRegulated,NXTConstants.motorRegulation.none,
       regulatedTurnRatio,NXTConstants.motorRunState.running,0);
   },
@@ -45,7 +45,7 @@ export default {
   async turnMotorDegrees(motorPort,degrees,forward) {
     const motorPowerPercent = (forward) ? 25 : 225; // 0 - 100 forwards 255 - 156 backwards (156 = 100% backwards)
     const regulatedTurnRatio = 0; // 0 - 100 forwards 255 - 156 backwards (156 = 100% backwards)
-    await NXT.runMotorCommand(motorPort,motorPowerPercent,
+    await NXTCommands.runMotorCommand(motorPort,motorPowerPercent,
       NXTConstants.motorModes.motorOnBreakRegulated,NXTConstants.motorRegulation.none,
       regulatedTurnRatio,NXTConstants.motorRunState.running,degrees);
   }

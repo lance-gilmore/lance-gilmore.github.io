995021c6d01b1dfa1e1064b8b2fa27f286dabb3d
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2023-11-22 09:41:41 +1000

    nn

--- device_status/sensor_config_component.js
+++ device_status/sensor_config_component.js
@@ -9,13 +9,13 @@ export default {
   <div class="p-2 info">
     <b>Sensor Config</b>
     <br>
-    Switch 1: {{ switch1Port }} 
+    Switch 1: {{ switch1Port + 1 }} 
     <br>
-    Switch 2: {{ switch2Port }}
+    Switch 2: {{ switch2Port + 1 }}
     <br>
-    Colour: {{ colourPort }}
+    Colour: {{ colourPort + 1 }}
     <br>
-    Ultrasonic: {{ ultrasonicPort }}
+    Ultrasonic: {{ ultrasonicPort + 1 }}
   </div>
   `
 }

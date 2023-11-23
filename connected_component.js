926be8dbe4256a12f93aa8956e3286b14df946a8
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2023-11-23 12:54:37 +1000

    nn

--- connected_component.js
+++ connected_component.js
@@ -26,31 +26,22 @@ export default {
     const inputPorts = {'switch1Port':switch1Port, 'switch2Port':switch2Port, 'colourPort':colourPort, 'ultrasonicPort':ultrasonicPort}
 
     
-    const sensorReadings = ref({})
-  
+    let sensorReadingsInit = new SensorReadings()
+    const sensorReadings = reactive(sensorReadingsInit)
+    sensorReadings.rand = Math.random()
 
-    const deviceReader = ref({})
-    const commandsNXT = ref({})
-    const simpleCommands = ref({})
-    const commandQueue = ref({})
+    const deviceReader = new NXTDeviceReader(props.connection, sensorReadings)
+    const commandsNXT = new NXTCommands(props.connection, deviceReader);
+    const simpleCommands = new NXTSimplifiedCommands(commandsNXT)
+    const commandQueue = new NXTCommandQueue(deviceReader)
     
-    
+    console.log(sensorReadings)  
     return { inputPorts, colourPort, commandsNXT, simpleCommands, commandQueue, sensorReadings }
   },
 
   props: {connection: {type: NXTConnection, required: true}},
 
   mounted() {
-    this.sensorReadings = new SensorReadings()
-    this.sensorReadings.rand = Math.random()
-
-    console.log(this.sensorReadings)  
-
-    this.deviceReader = new NXTDeviceReader(this.connection, this.sensorReadings)
-    this.commandsNXT = new NXTCommands(this.connection, this.deviceReader);
-    this.simpleCommands = new NXTSimplifiedCommands(this.commandsNXT)
-    this.commandQueue = new NXTCommandQueue(this.deviceReader)
-
     this.initSensors(this.commandsNXT, this.inputPorts)
   },
 

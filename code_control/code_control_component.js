905d04ab8ca88e0968be5801b262fa07195ff241
lance-gilmore <83896565+lance-gilmore@users.noreply.github.com>
2023-11-25 08:40:00 +1000

    nn

--- code_control/code_control_component.js
+++ code_control/code_control_component.js
@@ -21,6 +21,79 @@ export default {
 
   methods: {
       async runCode() {
+        let motorAMoving = false
+        function motorAForward() {
+          if (motorAMoving !== 'forward') {
+            motorAMoving = 'forward'
+            await this.simpleCommands.forwardMotor(NXTConstants.motors.PORT_A)
+          }
+        }
+        function motorABackward() {
+          if (motorAMoving !== 'backward') {
+            motorAMoving = 'backward'
+            await this.simpleCommands.backwardMotor(NXTConstants.motors.PORT_A)
+          }
+        }
+        function motorAStop() {
+          if (motorAMoving !== false) {
+            motorAMoving = false
+            await this.simpleCommands.stopMotor(NXTConstants.motors.PORT_A)
+          }
+        }
+
+        let motorBMoving = false
+        function motorBForward() {
+          if (motorBMoving !== 'forward') {
+            motorBMoving = 'forward'
+            await this.simpleCommands.forwardMotor(NXTConstants.motors.PORT_B)
+          }
+        }
+        function motorBBackward() {
+          if (motorBMoving !== 'backward') {
+            motorBMoving = 'backward'
+            await this.simpleCommands.backwardMotor(NXTConstants.motors.PORT_B)
+          }
+        }
+        function motorBStop() {
+          if (motorBMoving !== false) {
+            motorBMoving = false
+            await this.simpleCommands.stopMotor(NXTConstants.motors.PORT_B)
+          }
+        }
+
+        let motorCMoving = false
+        function motorCForward() {
+          if (motorCMoving !== 'forward') {
+            motorCMoving = 'forward'
+            await this.simpleCommands.forwardMotor(NXTConstants.motors.PORT_V)
+          }
+        }
+        function motorCBackward() {
+          if (motorCMoving !== 'backward') {
+            motorCMoving = 'backward'
+            await this.simpleCommands.backwardMotor(NXTConstants.motors.PORT_C)
+          }
+        }
+        function motorCStop() {
+          if (motorCMoving !== false) {
+            motorCMoving = false
+            await this.simpleCommands.stopMotor(NXTConstants.motors.PORT_C)
+          }
+        }
+
+        let beeping = false
+        function beep() {
+          if (!beeping) {
+            beeping = true
+            this.simpleCommands.simpleBeep()
+            setTimeout(function(){beeping = false},1000)
+          }
+        }
+        
+        let sensorReadings = this.sensorReadings
+
+
+
         this.running = true
         let code = this.$refs.codeEditor.editorCode
         eval(code);
@@ -41,30 +114,48 @@ export default {
   <button v-if="!running" type="btn" class="btn btn-primary" @click="runCode">run</button>
   <button v-if="running" type="btn" class="btn btn-primary" @click="stopCode">stop</button>
 
+  <h3>Commands</h3>
+  <ul>
+    <li>motorAForward()</li>
+    <li>motorABackward()</li>
+    <li>motorAStop()</li>
+    <li>motorBForward()</li>
+    <li>motorBBackward()</li>
+    <li>motorBStop()</li>
+    <li>motorCForward()</li>
+    <li>motorCBackward()</li>
+    <li>motorCStop()</li>
+    <li>beep()</li>
+    <li>sensorReadings.switch1</li>
+    <li>sensorReadings.switch2</li>
+    <li>sensorReadings.colour.colour <i>black, blue, green, yellow, red, white</i></li>
+    <li>sensorReadings.colour.scaledValue <i>0=dark, 1023=light</i></li>
+  </ul>
+
   <h3>Examples</h3>
   <div class="row" style="white-space: pre-wrap;">
     <b>Beep when switch1 is pressed</b>
 
     <p>
-    if (this.sensorReadings.switch1) {
-      this.simpleCommands.simpleBeep()
+    if (sensorReadings.switch1) {
+      beep()
     }
     </p>
 
-    
+
     <b>Drive motors BC with buttons</b>
 
     <p>
     while(running) {
-      if (this.sensorReadings.switch1) {
-        await this.simpleCommands.forwardMotor(NXTConstants.motors.PORT_B);
+      if (sensorReadings.switch1) {
+        motorBForward()
       } else {
-        await this.simpleCommands.stopMotor(NXTConstants.motors.PORT_B);
+        motorBStop()
       }
-      if (this.sensorReadings.switch2) {
-        await this.simpleCommands.forwardMotor(NXTConstants.motors.PORT_C);
+      if (sensorReadings.switch2) {
+        motorCForward()
       } else {
-        await this.simpleCommands.stopMotor(NXTConstants.motors.PORT_C);
+        motorCStop()
       }
     }
     </p>
